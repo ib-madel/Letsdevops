c319b30260cfb40507937350acf0479fffb2f34d
Moaaz Adel <113613641+ib-madel@users.noreply.github.com>
2022-09-29 18:56:51 +0200

    Update workflow_trigger.py

--- Deployment-Scripts/workflow_trigger.py
+++ Deployment-Scripts/workflow_trigger.py
@@ -3,12 +3,4 @@ import os
 import requests
 import sys
 
-TOKEN= str(sys.argv[1])
-OWNER= str(sys.argv[2])
-REPO= str(sys.argv[3])
-Workflow_Name= str(sys.argv[4])
-pl_Baseline_Number= str(sys.argv[5])
-pl_Baseline_Revision = str(sys.argv[6])
-
-
 print( "the toke value is")
